34aff257cebfdaac63c81a9b0ab447f838a3c1b2
chiheb guelmami <chiheb.guelmami@worldline.com>
2024-10-20 19:21:33 +0200

    change theme

---
+++ tournoi/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Fichier CSS pour styliser la barre de navigation
+
+const Navbar = () => {
+    return (
+        <AppBar position="static" sx={{ backgroundColor: '#2C3E50' }}> {/* Fond sombre élégant */}
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}> {/* Alignement des éléments */}
+
+
+
+                {/* Menu Horizontal */}
+                <Box sx={{ display: 'flex', gap: '30px' }}> {/* Espacement entre les boutons */}
+                    <Button component={Link} to="/" sx={{ color: '#ecf0f1', fontWeight: 'bold' }}>
+                        Accueil
+                    </Button>
+                    <Button component={Link} to="/group-stage" sx={{ color: '#ecf0f1', fontWeight: 'bold' }}>
+                        Phase de groupe
+                    </Button>
+                    <Button component={Link} to="/joueurs-inscrit" sx={{ color: '#ecf0f1', fontWeight: 'bold' }}>
+                        Joueurs Inscrits
+                    </Button>
+                    <Button component={Link} to="/sanctions" sx={{ color: '#ecf0f1', fontWeight: 'bold' }}>
+                        Sanctions
+                    </Button>
+                    <Button component={Link} to="/eliminatoire" sx={{ color: '#ecf0f1', fontWeight: 'bold' }}>
+                        Phase Éliminatoire
+                    </Button>
+                </Box>
+                {/* Logo ou Titre */}
+                <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#ecf0f1' }}>
+                    TIKTOK FOR ALL
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
