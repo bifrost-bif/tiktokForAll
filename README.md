30c5766f53fd18165bd0271897108da7e33b6ead
chiheb guelmami <chiheb.guelmami@worldline.com>
2024-10-21 05:58:14 +0200

    refactor projet build 2

---
+++ README.md
@@ -0,0 +1,98 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+## Calendrier des matchs
+
+Le composant **`Calendar`** affiche les journées du tournoi avec les matchs programmés, incluant les horaires et les scores des matchs déjà terminés.
+
+### Utilisation des scores
+
+- Si le score d'un joueur est **`-1`**, cela signifie que le match **n'a pas encore été joué**. Dans ce cas, le score ne sera **pas affiché** dans l'interface.
+- Une barre horizontale bicolore est utilisée pour représenter les scores une fois le match terminé :
+    - **Rouge** pour le premier joueur.
+    - **Bleu** pour le second joueur.
+    - Le joueur gagnant est discrètement mis en valeur par une légère surbrillance.
+
+### Exemple de structure de données
+
+Voici comment structurer les données des matchs :
+
+```javascript
+const calendarData = [
+    {
+        date: "Vendredi 1 NOV 2024",
+        matches: [
+            { player1: { name: "Joueur 1", score: 20 }, player2: { name: "Joueur 2", score: 15 }, time: "18:00" },
+            { player1: { name: "Joueur 3", score: -1 }, player2: { name: "Joueur 4", score: -1 }, time: "20:00" }
+        ]
+    },
+    ...
+];
---
+++ src/pages/GroupStage.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import { Grid, Table, TableBody, TableCell, TableHead, TableRow, Typography, Paper, Divider } from '@mui/material';
+import { styled } from '@mui/system';
+import data from '../data.json'; // Importer les données depuis le fichier JSON
+import './GroupStage.css'; // Fichier CSS pour styliser la page
+
+// Style personnalisé pour les drapeaux et le classement
+const Flag = styled('img')({
+    width: '80px',
+    height: '60px',
+    marginRight: '10px',
+    borderRadius: '4px',
+});
+
+const RankCircle = styled('div')(({ rank }) => ({
+    display: 'inline-block',
+    backgroundColor: rank >= 3 ? '#F44336' : '#4CAF50',  // Rouge pour les 3e et 4e, Vert pour les 1ers et 2èmes
+    color: 'white',
+    borderRadius: '50%',
+    width: '35px',
+    height: '35px',
+    textAlign: 'center',
+    lineHeight: '35px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    marginRight: '10px',
+}));
+
+const GroupStage = () => {
+    const groupsData = data.groups; // Accéder aux données des groupes depuis le fichier JSON
+
+    // Catégoriser les groupes
+    const newPlayerGroups = groupsData.slice(0, 4); // Groupes A à D
+    const oldPlayerGroups = groupsData.slice(4);    // Groupes E à H
+
+    return (
+        <div className="group-stage-container">
+            {/* Section des Nouveaux Joueurs */}
+            <Typography variant="h4" className="category-title">Nouveaux Joueurs</Typography>
+            <Grid container spacing={4} className="group-container">
+                {newPlayerGroups.map((group, index) => (
+                    <Grid item xs={12} sm={6} key={index}>
+                        <Paper className="group-paper">
+                            <Typography variant="h6" className="group-title">{group.groupName}</Typography>
+
+                            {/* Conteneur avec défilement horizontal */}
+                            <div className="table-container">
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center" className="table-header">Classement</TableCell>
+                                            <TableCell align="center" className="table-header"></TableCell>
+                                            <TableCell align="center" className="table-header">Joueur</TableCell>
+                                            <TableCell align="center" className="table-header">MJ</TableCell>
+                                            <TableCell align="center" className="table-header">Coins</TableCell>
+                                            <TableCell align="center" className="table-header">Points</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {group.teams
+                                            .slice()
+                                            .sort((a, b) => b.points - a.points)
+                                            .map((team, idx) => (
+                                                <TableRow key={team.id}>
+                                                    <TableCell align="center">
+                                                        <RankCircle rank={idx + 1}>{idx + 1}</RankCircle>
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        <Flag src={team.flag} alt={team.name} />
+                                                    </TableCell>
+                                                    <TableCell align="center">{team.name}</TableCell>
+                                                    <TableCell align="center">{team.played}</TableCell>
+                                                    <TableCell align="center">{team.coins}</TableCell>
+                                                    <TableCell align="center">{team.points}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                    </TableBody>
+                                </Table>
+                            </div>
+                        </Paper>
+                    </Grid>
+                ))}
+            </Grid>
+
+            {/* Séparateur élégant */}
+            <Divider variant="middle" className="group-divider" />
+
+            {/* Section des Anciens Joueurs */}
+            <br/>
+            <Typography variant="h4" className="category-title">Anciens Joueurs</Typography>
+            <Grid container spacing={4} className="group-container">
+                {oldPlayerGroups.map((group, index) => (
+                    <Grid item xs={12} sm={6} key={index}>
+                        <Paper className="group-paper">
+                            <Typography variant="h6" className="group-title">{group.groupName}</Typography>
+
+                            {/* Conteneur avec défilement horizontal */}
+                            <div className="table-container">
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center" className="table-header">#</TableCell>
+                                            <TableCell align="center" className="table-header"></TableCell>
+                                            <TableCell align="center" className="table-header">Joueur</TableCell>
+                                            <TableCell align="center" className="table-header">Matchs</TableCell>
+                                            <TableCell align="center" className="table-header">Coins</TableCell>
+                                            <TableCell align="center" className="table-header">Points</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {group.teams
+                                            .slice()
+                                            .sort((a, b) => b.points - a.points)
+                                            .map((team, idx) => (
+                                                <TableRow key={team.id}>
+                                                    <TableCell align="center">
+                                                        <RankCircle rank={idx + 1}>{idx + 1}</RankCircle>
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        <Flag src={team.flag} alt={team.name} />
+                                                    </TableCell>
+                                                    <TableCell align="center">{team.name}</TableCell>
+                                                    <TableCell align="center">{team.played}</TableCell>
+                                                    <TableCell align="center">{team.coins}</TableCell>
+                                                    <TableCell align="center">{team.points}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                    </TableBody>
+                                </Table>
+                            </div>
+                        </Paper>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
+};
+
+export default GroupStage;
