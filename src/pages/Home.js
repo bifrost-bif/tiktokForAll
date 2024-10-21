58f5b1ab09bb887daeacf92fb97a3bb23dd474a7
chiheb guelmami <chiheb.guelmami@worldline.com>
2024-10-21 02:56:58 +0200

    rollback tiktok bifrost

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,37 +3,14 @@ import { Typography, Box } from '@mui/material';
 import './home.css';
 
 const Home = () => {
-    // Load the TikTok embed script
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://www.tiktok.com/embed.js';
-        script.async = true;
-        document.body.appendChild(script);
-    }, []);
-
     return (
         <div className="group-stage-container">
-            {/* Section for tournament information */}
-            <Typography variant="h5">
-                Sélectionnez une phase pour voir les groupes ou les éliminations directes.
-            </Typography>
-
-            {/* TikTok Embed */}
-            <blockquote
-                className="tiktok-embed"
-                cite="https://www.tiktok.com/@bif.bif"
-                data-unique-id="bif.bif"
-                data-embed-type="creator"
-                style={{ maxWidth: '780px', minWidth: '288px' }} // Corrected style prop
-            >
-                <section>
-                    <a target="_blank" href="https://www.tiktok.com/@bif.bif?refer=creator_embed">
-                        @bif.bif
-                    </a>
-                </section>
-            </blockquote>
+            {/* Section pour l'affiche du tournoi */}
+            <Typography variant="h5">Sélectionnez une phase pour voir les groupes ou les éliminations
+                directes.</Typography>
+            <img src="/images/affiche.png" alt="Affiche du Tournoi" className="tournoi-image"/>
         </div>
-    );
+    )
 };
 
 export default Home;
