bcd939b83e12a212f2f6ccd1861a854cfc88eb87
chiheb guelmami <chiheb.guelmami@worldline.com>
2024-10-25 21:50:49 +0200

    add Content-Security-Policy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,6 @@ import { Typography, Box, Grid, Paper } from '@mui/material';
 import './Home.css';
 
 const Home = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://www.tiktok.com/embed.js';
-        script.async = true;
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
 
     return (
         <div className="home-container">
