6d6cf42024abad4cf6672c1f10a6694dfcfead17
chiheb guelmami <chiheb.guelmami@worldline.com>
2024-11-04 23:26:19 +0100

    secure: delete Admin

--- src/App.js
+++ src/App.js
@@ -10,15 +10,12 @@ import Navbar from './components/Navbar';
 import Calendar from "./pages/Calendrier";
 
 import Footer from './components/Footer';
-import AdminCalendar from './components/AdminCalendar';
 import PolitiqueCookies from './pages/PolitiqueCookies';
 import PolitiqueConfidentialite from './pages/PolitiqueConfidentialite';
 import MentionsLegales from './pages/MentionsLegales';
 import ConditionsUtilisation from './pages/ConditionsUtilisation';
 import ContactDonnees from './pages/ContactDonnees';
 import Mosammin from './pages/Mosammin'; // Import the new Mosammin page
-import AdminPage from './components/AdminPage';
-import ProtectedRoute from './components/ProtectedRoute';
 
 import './App.css'; // Fichier CSS pour styliser la page
 
@@ -81,15 +78,8 @@ function App() {
                     <Route path="/conditions-utilisation" element={<ConditionsUtilisation/>}/>
                     <Route path="/contact-donnees" element={<ContactDonnees/>}/>
                     <Route path="/mosammin" element={<Mosammin />} />
-                    <Route path="/login" element={<AdminPage />} />
-            <Route
-                path="/adminCalendar"
-                element={<ProtectedRoute element={<AdminCalendar />} />}
-            />
-            {/* Ajoutez d'autres routes ici si nécessaire */}
-        </Routes>
-                
-                <Footer/> {/* Ajout du footer */}
+                </Routes>
+                <Footer/> 
             </div>
         </Router>
     );
